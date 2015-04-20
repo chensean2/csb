3ea6226847afbe24e2beb4a06433a9b8086ed36f
gengjun <gengjunlover@gmail.com>
2015-04-20 22:25:59 +0800

    provision change

--- saas-provision/src/main/java/com/saas/entity/ProvisionServiceRes.java
+++ saas-provision/src/main/java/com/saas/entity/ProvisionServiceRes.java
@@ -15,161 +15,120 @@ import javax.persistence.TemporalType;
 
 import com.saas.common.entity.BaseEntity;
 
-
 /**
  * The persistent class for the tbl_ss_provision_service_res database table.
  * 
  */
 @Entity
-@Table(name="tbl_ss_provision_service_res")
+@Table(name = "tbl_ss_provision_service_res")
 public class ProvisionServiceRes extends BaseEntity<Long> {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@OneToOne
-	@JoinColumn(name="ISV_SERVICE_ID")
-	private ProvisionIsvService provisionIsvService;
+    @OneToOne
+    @JoinColumn(name = "ISV_SERVICE_ID")
+    private ProvisionIsvService provisionIsvService;
 
-	@OneToMany(mappedBy="provisionServiceRes")
-	private List<ProvisionServiceResDetail> provisionServiceResDetailList;
-	
-	@Column(name="PROVISION_RES_CODE")
-	private String provisionResCode;
+    @OneToMany(mappedBy = "provisionServiceRes")
+    private List<ProvisionServiceResDetail> provisionServiceResDetailList;
 
-	@Lob
-	@Column(name="PROVISION_RES_MESSAGE")
-	private String provisionResMessage;
+    @Column(name = "PROVISION_RES_CODE")
+    private String provisionResCode;
 
-	@Column(name="ROLLBACK_RES_CODE")
-	private String rollbackResCode;
+    @Lob
+    @Column(name = "PROVISION_RES_MESSAGE")
+    private String provisionResMessage;
 
-	@Lob
-	@Column(name="ROLLBACK_RES_MESSAGE")
-	private String rollbackResMessage;
+    @Column(name = "ROLLBACK_RES_CODE")
+    private String rollbackResCode;
 
-	@Lob
-	@Column(name="SERVICE_RES_MESSAGE")
-	private String serviceResMessage;
+    @Lob
+    @Column(name = "ROLLBACK_RES_MESSAGE")
+    private String rollbackResMessage;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name="SERVICE_RES_RECEIVE_TIME")
-	private Date serviceResReceiveTime;
+    @Lob
+    @Column(name = "SERVICE_RES_MESSAGE")
+    private String serviceResMessage;
 
-	@Column(name="SERVICE_RES_TYPE")
-	private String serviceResType;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "SERVICE_RES_RECEIVE_TIME")
+    private Date serviceResReceiveTime;
 
+    @Column(name = "SERVICE_RES_TYPE")
+    private String serviceResType;
 
-	public ProvisionServiceRes() {
-	}
+    public ProvisionServiceRes() {
+    }
 
+    public String getProvisionResCode() {
+        return provisionResCode;
+    }
 
+    public void setProvisionResCode(String provisionResCode) {
+        this.provisionResCode = provisionResCode;
+    }
 
+    public String getRollbackResCode() {
+        return rollbackResCode;
+    }
 
-	public String getProvisionResCode() {
-		return provisionResCode;
-	}
+    public void setRollbackResCode(String rollbackResCode) {
+        this.rollbackResCode = rollbackResCode;
+    }
 
+    public Date getServiceResReceiveTime() {
+        return serviceResReceiveTime;
+    }
 
-	public void setProvisionResCode(String provisionResCode) {
-		this.provisionResCode = provisionResCode;
-	}
+    public void setServiceResReceiveTime(Date serviceResReceiveTime) {
+        this.serviceResReceiveTime = serviceResReceiveTime;
+    }
 
+    public String getServiceResType() {
+        return serviceResType;
+    }
 
-	public String getRollbackResCode() {
-		return rollbackResCode;
-	}
+    public void setServiceResType(String serviceResType) {
+        this.serviceResType = serviceResType;
+    }
 
+    public ProvisionIsvService getProvisionIsvService() {
+        return provisionIsvService;
+    }
 
-	public void setRollbackResCode(String rollbackResCode) {
-		this.rollbackResCode = rollbackResCode;
-	}
+    public void setProvisionIsvService(ProvisionIsvService provisionIsvService) {
+        this.provisionIsvService = provisionIsvService;
+    }
 
+    public List<ProvisionServiceResDetail> getProvisionServiceResDetailList() {
+        return provisionServiceResDetailList;
+    }
 
+    public void setProvisionServiceResDetailList(List<ProvisionServiceResDetail> provisionServiceResDetailList) {
+        this.provisionServiceResDetailList = provisionServiceResDetailList;
+    }
 
-	public Date getServiceResReceiveTime() {
-		return serviceResReceiveTime;
-	}
+    public String getProvisionResMessage() {
+        return provisionResMessage;
+    }
 
+    public void setProvisionResMessage(String provisionResMessage) {
+        this.provisionResMessage = provisionResMessage;
+    }
 
-	public void setServiceResReceiveTime(Date serviceResReceiveTime) {
-		this.serviceResReceiveTime = serviceResReceiveTime;
-	}
+    public String getRollbackResMessage() {
+        return rollbackResMessage;
+    }
 
+    public void setRollbackResMessage(String rollbackResMessage) {
+        this.rollbackResMessage = rollbackResMessage;
+    }
 
-	public String getServiceResType() {
-		return serviceResType;
-	}
+    public String getServiceResMessage() {
+        return serviceResMessage;
+    }
 
-
-	public void setServiceResType(String serviceResType) {
-		this.serviceResType = serviceResType;
-	}
-
-
-	public ProvisionIsvService getProvisionIsvService() {
-		return provisionIsvService;
-	}
-
-
-	public void setProvisionIsvService(ProvisionIsvService provisionIsvService) {
-		this.provisionIsvService = provisionIsvService;
-	}
-
-
-
-
-	public List<ProvisionServiceResDetail> getProvisionServiceResDetailList() {
-		return provisionServiceResDetailList;
-	}
-
-
-
-
-	public void setProvisionServiceResDetailList(
-			List<ProvisionServiceResDetail> provisionServiceResDetailList) {
-		this.provisionServiceResDetailList = provisionServiceResDetailList;
-	}
-
-
-
-
-	public String getProvisionResMessage() {
-		return provisionResMessage;
-	}
-
-
-
-
-	public void setProvisionResMessage(String provisionResMessage) {
-		this.provisionResMessage = provisionResMessage;
-	}
-
-
-
-
-	public String getRollbackResMessage() {
-		return rollbackResMessage;
-	}
-
-
-
-
-	public void setRollbackResMessage(String rollbackResMessage) {
-		this.rollbackResMessage = rollbackResMessage;
-	}
-
-
-
-
-	public String getServiceResMessage() {
-		return serviceResMessage;
-	}
-
-
-
-
-	public void setServiceResMessage(String serviceResMessage) {
-		this.serviceResMessage = serviceResMessage;
-	}
-
+    public void setServiceResMessage(String serviceResMessage) {
+        this.serviceResMessage = serviceResMessage;
+    }
 
 }
