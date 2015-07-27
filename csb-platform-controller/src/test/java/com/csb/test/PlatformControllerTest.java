77dc905f6fd4c0c2ac5396fc19a1e91d502fbbc0
gengjun <gengjunlover@gmail.com>
2015-07-27 23:41:15 +0800

    design

--- csb-platform-controller/src/test/java/com/csb/test/PlatformControllerTest.java
+++ csb-platform-controller/src/test/java/com/csb/test/PlatformControllerTest.java
@@ -1,20 +1,20 @@
 package com.csb.test;
 
-import java.io.StringReader;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamReader;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.w3c.dom.DocumentType;
 
+import com.csb.common.manifest.CordsImage;
+import com.csb.common.manifest.CordsInfrastructure;
 import com.csb.common.manifest.CordsManifest;
-import com.csb.common.util.JaxbUpperCaseStreamReaderDelegate;
+import com.csb.common.manifest.CordsNode;
 import com.csb.parser.component.model.IaaSInfo;
 import com.csb.parser.component.model.SubscriptionInfo;
 import com.csb.parser.component.model.SubscriptionResult;
@@ -61,20 +61,51 @@ public class PlatformControllerTest extends BaseIT {
 
 	
 		try {
-			Path path = FileSystems.getDefault().getPath("C:\\Users\\gengjun\\Downloads", "xwiki.xml");
+			//Path path = FileSystems.getDefault().getPath("/Users/gengjun/dev/saas-base-workspace", "xwiki.xml");
 			
-			String manifestStr = new String(Files.readAllBytes(path));
+			//String manifestStr = new String(Files.readAllBytes(path));
+		    
+		    
 			CordsManifest manifest = null;
 			JAXBContext jc;
 
-			jc = JAXBContext.newInstance("com.csb.common.manifest");
+			jc = JAXBContext.newInstance(com.csb.common.manifest.ObjectFactory.class);
 
-			XMLInputFactory xif = XMLInputFactory.newInstance();
-			StringReader reader = new StringReader(manifestStr);
-			XMLStreamReader xsr = xif.createXMLStreamReader(reader);
-			xsr = new JaxbUpperCaseStreamReaderDelegate(xsr);
-			manifest = (CordsManifest) jc.createUnmarshaller().unmarshal(xsr);
+			Unmarshaller unmarshaller = jc.createUnmarshaller(); 
+			manifest = (CordsManifest)((JAXBElement<DocumentType>) unmarshaller.unmarshal(new File("/Users/gengjun/dev/saas-base-workspace/xwiki.xml"))).getValue(); 
 			System.out.println(manifest);
+			CordsNode node = manifest.getNode().get(0);
+			String provider = node.getProvider();
+			CordsImage image = node.getImage();
+			String instanceName = node.getName();
+			CordsInfrastructure infra = node.getInfrastructure(); 
+			SubscriptionInfo s = new SubscriptionInfo();
+			
+	                s.setTraceId("00001");
+	                s.setAppPlanId("iaas-plan-00001");
+	                s.setCategory("IAAS");
+
+	                IaaSInfo iaasInfo = new IaaSInfo();
+	                iaasInfo.setProvider(provider);
+	                iaasInfo.setAction("CREATE");
+	                iaasInfo.setCpus(Integer.parseInt(infra.getCompute().getCores()));
+	                iaasInfo.setInstanceName(instanceName);
+	                iaasInfo.setMemory(infra.getCompute().getMemory().toLowerCase()+"b");
+	                iaasInfo.setImageName(image.getSystem().getName());
+	                iaasInfo.setStorage(infra.getStorage().getSize().toLowerCase()+"b");
+	                // m1.1gb.1cpu.10gb
+	                iaasInfo.setFlavor(
+	                                "m1." + iaasInfo.getMemory() + "." + iaasInfo.getCpus() + "cpu." + iaasInfo.getStorage());
+	                s.setIaasInfo(iaasInfo);
+	                SubscriptionResult result = controllerService.createSubscription(s);
+
+	                if (result.getEventId() != null) {
+	                        csbBrokerService.broke(result.getEventId());
+	                }
+	                
+			
+			
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
