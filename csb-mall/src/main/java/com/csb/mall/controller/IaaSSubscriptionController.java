3a8b3ca7a68c102fda0f550a0c49a6f06a80df16
gengjun <gengjunlover@gmail.com>
2015-08-07 13:42:16 +0800

    design

--- csb-mall/src/main/java/com/csb/mall/controller/IaaSSubscriptionController.java
+++ csb-mall/src/main/java/com/csb/mall/controller/IaaSSubscriptionController.java
@@ -1,16 +1,14 @@
 package com.csb.mall.controller;
 
-import java.net.URI;
-
 import javax.validation.Validator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,7 +37,17 @@ public class IaaSSubscriptionController {
 	@Autowired
 	private Validator validator;
 
-
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	@ResponseBody
+	public PocIaaSInfoTO get(@PathVariable("id") Long id) {
+		PocIaaSInfoTO iaas = new PocIaaSInfoTO();
+		iaas.setCpus(1);
+		iaas.setInstanceName("test1");
+		iaas.setImageName("Debian-with-passwd");
+		iaas.setMemory("1G");
+		iaas.setStorage("10G");
+		return iaas;
+	}
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaTypes.JSON)
 	@ResponseBody
 	public ResponseEntity<?> create(@RequestBody PocIaaSInfoTO iaas) {
@@ -60,7 +68,7 @@ public class IaaSSubscriptionController {
 		iaasInfo.setMemory(iaas.getMemory().toLowerCase() + "b");
 		//Debian-with-passwd
 		iaasInfo.setImageName(iaas.getImageName());
-		//1G
+		//10G
 		iaasInfo.setStorage(iaas.getStorage().toLowerCase() + "b");
 		// m1.1gb.1cpu.10gb
 		iaasInfo.setFlavor(
