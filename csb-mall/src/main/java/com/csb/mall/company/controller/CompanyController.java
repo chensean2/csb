864e0626be21598f7bffd403e637ee0c3306fbc6
gengjun <gengjunlover@gmail.com>
2015-10-07 21:57:51 +0800

    design for platform

--- csb-mall/src/main/java/com/csb/mall/company/controller/CompanyController.java
+++ csb-mall/src/main/java/com/csb/mall/company/controller/CompanyController.java
@@ -2,12 +2,12 @@ package com.csb.mall.company.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/company")
 public class CompanyController {
 
